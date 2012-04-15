ff322a012ca68584869aaaecc96ecb10661532cf
Master Pie <vrp@andrew.cmu.edu>
2012-04-15 12:53:48 -0400

    Fixing webiso checkin part 2

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   if Rails.env == "production"
-    provider :CMU, :host => 'teudu.andrew.cmu.edu', :appid => 'teudu.andrew.cmu.edu'', :keyfile => '/usr/local/pubcookie/keys/teudu.andrew.cmu.edu', :granting_cert => '/usr/local/pubcookie/keys/pubcookie_granting.cert'
+    provider :CMU, :host => 'teudu.andrew.cmu.edu', :appid => 'teudu.andrew.cmu.edu', :keyfile => '/usr/local/pubcookie/keys/teudu.andrew.cmu.edu', :granting_cert => '/usr/local/pubcookie/keys/pubcookie_granting.cert'
   end
 end
